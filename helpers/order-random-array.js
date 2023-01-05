928d194bf9aa1348dc4a0082a787581e9c151e6a
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-01-04 18:41:17 -0600

    feat: se agregan helpers de ordenamiento

---
+++ helpers/order-random-array.js
@@ -0,0 +1,6 @@
+
+
+// Ordena aleatoriamente un array
+const numbers = [1,5,8,9,3];
+
+numbers.sort((a, b) => Math.random() - 0.5);
---
+++ helpers/random-item-array.js
@@ -0,0 +1,8 @@
+// Recupera aleatoriamente un item del array
+const numbers = [1,5,8,9,3];
+
+const randomIndex = Math.floor(
+  Math.random() * numbers.length
+)
+
+numbers[randomIndex];
